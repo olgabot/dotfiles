38e9d7a838f2c01b35a23d648ddc52f11f7e788e
Olga Botvinnik <olga.botvinnik@gmail.com>
2018-05-09 10:19:40 -0700

    Explicit paths

--- init.sh
+++ init.sh
@@ -1,12 +1,10 @@
 sudo apt install --yes zsh emacs tree
 sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-mkdir -p ~/code
-cd ~/code
-git clone https://github.com/zakaziko99/agnosterzak-ohmyzsh-theme
-git clone https://github.com/edran/hc-zenburn-emacs
+git clone https://github.com/zakaziko99/agnosterzak-ohmyzsh-theme ~/code/agnosterzak-ohmyzsh-theme
+git clone https://github.com/edran/hc-zenburn-emacs ~/code/hc-zenburn-emacs
 
 cp -v ~/code/rcfiles/.* ~/
-cp -vr rcfiles/.emacs.d ~/
+cp -vr ~/code/rcfiles/.emacs.d ~/
 cp -v ~/code/agnosterzak-ohmyzsh-theme/agnosterzak.zsh-theme ~/.oh-my-zsh/themes
 cp -v ~/code/hc-zenburn-emacs/hc-zenburn-theme.el ~/.emacs.d/themes
